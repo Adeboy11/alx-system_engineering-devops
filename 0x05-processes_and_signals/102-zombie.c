00549a8d2c5e20f37538389ef9d106e3b11aecd6
Adeboy11 <princeadeboy4real1@gmail.com>
2023-12-22 02:41:09 -0800

    signal

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+/**
+ * infinite_while - a func to run forever and returns nothing
+ * Return: 0 on success
+*/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - the entry point to a prog to create 5 zombie processes
+ * Return: 0 on success
+*/
+int main(void)
+{
+	int children_processes = 0;
+	pid_t pid;
+
+	while (children_processes < 5)
+	{
+		pid = fork();
+		if (!pid)
+			break;
+		printf("Zombie process created, PID: %i\n", (int)pid);
+		children_processes++;
+	}
+	if (pid != 0)
+		infinite_while();
+	return (0);
+}
